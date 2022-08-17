bad7e323570944b82c82073f394d15559a088ff8
mgurung01 <gurungmandeep01@gmail.com>
2022-08-17 14:39:19 -0500

    Certain changes

--- Print.java
+++ Print.java
@@ -3,8 +3,10 @@ public class Print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hellow World");
-
+		int x = 2;
+		int y = 3;
+		float z = x*y;
+		System.out.print("The total of this multipliation is:"+z);
 	}
 
 }
