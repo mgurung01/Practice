6cb40f1ffc9607ebf2c31e7c69b3cd42a6c75e1d
mgurung01 <gurungmandeep01@gmail.com>
2022-08-17 14:05:21 -0500

    New Practice

---
+++ Mattgurung.java
@@ -0,0 +1,13 @@
+
+public class Mattgurung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float x = 2;
+		float y = 20;
+		float divide = x/y;
+		System.out.print("The division of x divided by y is:"+divide);
+		
+	}
+
+}
